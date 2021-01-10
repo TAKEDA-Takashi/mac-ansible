d9fdefc80cf933489bd6487614076918d2a81982
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2021-01-10 13:44:20 +0900

    bashのgitコンプリートを明示的に追加（なぜかきかなくなった）

--- roles/homebrew/files/bashrc/50-bash-completion.sh
+++ roles/homebrew/files/bashrc/50-bash-completion.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
+
 [[ -r "$(brew --prefix)/etc/profile.d/bash_completion.sh" ]] && . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
+
+. "$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
